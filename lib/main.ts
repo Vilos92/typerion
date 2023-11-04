53ebba5d5e5106fe618b36c980d211d8b58ddea9
Gregory Linscheid <linscheid.greg@gmail.com>
2023-11-04 14:26:25 -0700

    :wrench: Tailwind

--- lib/main.ts
+++ lib/main.ts
@@ -2,10 +2,6 @@ import {Button as ButtonCmp} from './components/Button';
 import {Pad as PadCmp} from './components/Pad';
 import {withBaseStyles} from './helpers/twin';
 
-export function helloAnything(thing: string): string {
-  return `Hello ${thing}!`;
-}
-
 const Button = withBaseStyles(ButtonCmp);
 const Pad = withBaseStyles(PadCmp);
 
---
+++ tailwind.config.mjs
@@ -0,0 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}', './lib/**/*.{js,jsx,ts,tsx}'],
+  plugins: []
+};
