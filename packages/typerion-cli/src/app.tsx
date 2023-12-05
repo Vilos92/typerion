16b3146bab38c9a4c4a63c5f607886c9f35c22ca
Gregory Linscheid <linscheid.greg@gmail.com>
2023-12-04 23:06:18 -0800

    :wrench: Setup prettier ignore files

---
+++ packages/typerion-cli/src/app.tsx
@@ -0,0 +1,8 @@
+import {type FC} from 'react';
+import ReactDOM from 'react-dom';
+
+const App: FC = () => {
+  return <div>Hello, esbuild! </div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
