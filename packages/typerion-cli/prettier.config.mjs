5512d9a30eac1bc89cb133f020eecf2fe2c6f6ff
Gregory Linscheid <linscheid.greg@gmail.com>
2023-12-04 22:34:48 -0800

    :shirt: Add lint/prettier to new client package

---
+++ packages/typerion-cli/prettier.config.mjs
@@ -0,0 +1,17 @@
+import {createRequire} from 'node:module';
+
+const require = createRequire(import.meta.url);
+
+export default {
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  printWidth: 110,
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports'), 'prettier-plugin-tailwindcss'],
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+};
--- packages/typerion-cli/src/index.ts
+++ packages/typerion-cli/src/index.ts
@@ -1,21 +1,21 @@
-import Fastify from "fastify";
+import Fastify from 'fastify';
 
 const fastify = Fastify({
-  logger: true,
+  logger: true
 });
 
-fastify.get("/", async (_request, reply) => {
-  reply.type("application/json").code(200);
-  return { hello: "world" };
+fastify.get('/', async (_request, reply) => {
+  reply.type('application/json').code(200);
+  return {hello: 'world'};
 });
 
-fastify.listen({ port: 3000 }, (err, _address) => {
+fastify.listen({port: 3000}, (err, _address) => {
   if (err) throw err;
 });
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({port: 3000});
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
