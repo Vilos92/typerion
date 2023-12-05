988954a1011146a231f0277a2dba6a6dd6c8872f
Gregory Linscheid <linscheid.greg@gmail.com>
2023-12-04 22:24:20 -0800

    :wrench: Introduce typerion-cli package

---
+++ packages/typerion-cli/src/index.ts
@@ -0,0 +1,25 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (_request, reply) => {
+  reply.type("application/json").code(200);
+  return { hello: "world" };
+});
+
+fastify.listen({ port: 3000 }, (err, _address) => {
+  if (err) throw err;
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start().catch(console.error);
