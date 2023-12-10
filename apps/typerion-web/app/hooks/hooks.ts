d9f0863f2d834adbfb5ec69a9b87b7834e7caf42
Gregory Linscheid <linscheid.greg@gmail.com>
2023-12-10 14:35:14 -0800

    :bulb: We have the web app rendering

---
+++ apps/typerion-web/app/hooks/hooks.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+/*
+ * Hooks.
+ */
+
+export function useIsMounted() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return isMounted;
+}
