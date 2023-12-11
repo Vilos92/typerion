be05f15b59014935405043b12f702c58e430545f
Gregory Linscheid <linscheid.greg@gmail.com>
2023-12-11 00:10:59 -0800

    :art: New footer and logos for the web app

--- apps/typerion-web/app/routes/_index.tsx
+++ apps/typerion-web/app/routes/_index.tsx
@@ -1,8 +1,12 @@
+import {Link} from '@remix-run/react';
 import type {MetaFunction} from '@vercel/remix';
+import githubLogo from '~/assets/githubLogo.svg';
+import githubLogoDark from '~/assets/githubLogoDark.svg';
+import npmLogo from '~/assets/npmLogo.svg';
 import {Notebook} from '~/components/Notebook.client';
 import {useIsMounted} from '~/hooks/hooks';
 
-import {mainStyle} from './index.css';
+import {bottomNavButtonStyle, bottomNavStyle, logoImgStyle, mainStyle} from './index.css';
 
 export const meta: MetaFunction = () => {
   return [
@@ -17,5 +21,26 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   const isMounted = useIsMounted();
 
-  return <main className={mainStyle}>{isMounted ? <Notebook /> : null}</main>;
+  return (
+    <main className={mainStyle}>
+      {isMounted ? <Notebook /> : null}
+      <nav className={bottomNavStyle}>
+        <Link to="https://github.com/Vilos92/typerion">
+          <button className={bottomNavButtonStyle}>
+            <picture>
+              <source srcSet={githubLogo} media="(prefers-color-scheme: light)" />
+              <img className={logoImgStyle} src={githubLogoDark} alt="GitHub logo" />
+            </picture>
+            GitHub
+          </button>
+        </Link>
+        <Link to="https://www.npmjs.com/package/typerion">
+          <button className={bottomNavButtonStyle}>
+            <img className={logoImgStyle} src={npmLogo} alt="npm logo" />
+            npm
+          </button>
+        </Link>
+      </nav>
+    </main>
+  );
 }
--- apps/typerion-web/app/routes/index.css.ts
+++ apps/typerion-web/app/routes/index.css.ts
@@ -7,8 +7,47 @@ import {style} from '@vanilla-extract/css';
 export const mainStyle = style({
   width: '100%',
   paddingTop: '16px',
+  paddingBottom: '48px',
 
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center'
+});
+
+export const bottomNavStyle = style({
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+
+  height: '32px',
+  width: '100%',
+  paddingRight: '16px',
+
+  background: 'rgb(68 64 60)',
+  color: '#FFFFFF',
+
+  '@media': {
+    '(prefers-color-scheme: light)': {
+      backgroundColor: 'rgb(214 211 209)',
+      color: '#000000'
+    }
+  },
+
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  gap: '16px'
+});
+
+export const bottomNavButtonStyle = style({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: '4px'
+});
+
+export const logoImgStyle = style({
+  height: '24px',
+  width: 'auto'
 });
